47272b1d291bb3ee3fbce753d5bd8f35563a7ca1
LiuYanBo <284323968@qq.com>
2022-05-25 20:38:19 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
-
+from selenium.webdriver.common.keys import Keys
+import time
 class NewVisitorTest(unittest.TestCase):                #(1)
 	def setUp(self):
 		self.browser = webdriver.Chrome()
@@ -15,19 +16,35 @@ class NewVisitorTest(unittest.TestCase):                #(1)
 		self.browser.get('http://localhost:8000')      
 
 		#She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title), "Browser title was: " + self.browser.title
-		self.fail('Finish the test!')
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
 
 		#She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 
 		#She types "Buy peacock feathers" into a text box (Edith's hobby
 		#is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
 
 		#when she hits enter, the page updates, and now the page lists
 		#"1: Buy peacock feathers" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+			)
 
 		#There is still a text box inviting her to add another item. She
 		#enters "Use peacock feathers to make a fly"(Edith is very methodical)
+		self.fail('Finish the test!')
 
 		#The page updates again, and now shows both items on her list
 
@@ -37,6 +54,5 @@ class NewVisitorTest(unittest.TestCase):                #(1)
 
 		#She visits that URL - her to-do list is still there.
 		#Satisfied, she goes back to sleep
-
 if __name__ == '__main__':
 	unittest.main()
